ae3dbe36fd1d3dbc8aa4e1230d537a5cf67a0a95
Nicolas Debray <97224385+chmick15@users.noreply.github.com>
2022-04-26 09:00:13 +0200

    button home ok/color bg ok

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Movies from './pages/Movies';
 import Home from './pages/Home';
 import Mvp from './pages/Mvp'
 import Page404 from './pages/Page404';
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Projects from './pages/Projects';
 
@@ -13,7 +13,6 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <div className='h-screen bg-slate-300'>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='projects' element={<Projects />} />
@@ -21,8 +20,6 @@ function App() {
           <Route path='/project/MVP' element={<Mvp />} />
           <Route path='*' element={<Page404 />} />
         </Routes>
-        <Link to='/'>Return to the home page</Link>
-      </div>
     </BrowserRouter>
   );
 }
