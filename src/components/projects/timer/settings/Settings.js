341966335d025fbc8031d5082c638f4041054f53
Nicolas Debray <97224385+chmick15@users.noreply.github.com>
2022-05-02 23:55:30 +0200

    ok note

--- src/components/projects/timer/settings/Settings.js
+++ src/components/projects/timer/settings/Settings.js
@@ -3,12 +3,11 @@ import React, { useState } from "react";
 const Settings = () => {
 
     const title = `Eso es el titulo`
-    const [array, setArray] = useState(['Flavie', 'Pierre', 'Nicolas', 'Mathilde', 'Valerie', 'Christophe']);
+    const [clients, setClients] = useState([{ id: 1, nom: 'Flavie' }, { id: 2, nom: 'Pierre' }, { id: 3, nom: 'Nicolas' }, { id: 4, nom: 'Mathilde' }, { id: 5, nom: 'Valerie' }, { id: 6, nom: 'Christophe' }]);
     const [newUser, setNewUser] = useState('');
 
-    const handleDelete = (perso, index) => {
-        setArray(array.filter(perso => (index !== perso)))
-        console.log(array);
+    const handleDelete = (id) => {
+        console.log(id);
     }
 
     const handleChange = (event) => {
@@ -17,8 +16,6 @@ const Settings = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        array.push(newUser)
-        setNewUser('')
     }
 
     return (
@@ -26,7 +23,7 @@ const Settings = () => {
             <h1 className=" bg-pink-500 font-extrabold text-6xl">{title}</h1>
             <button className="bg-red-500 border-4">Click me</button>
             <ul>
-                {array.length > 0 ? array.map((perso, index) => <li key={index}>{perso} <button onClick={() => handleDelete(index, perso)}>X</button> </li>) : <p>rien</p>}
+                {clients.length > 0 ? clients.map((client, index) => <li key={index}>{client.nom} <button onClick={() => handleDelete(client.id)}>X</button> </li>) : <p>rien</p>}
             </ul>
             <form>
                 <input placeholder="Input" type='text' value={newUser} onChange={handleChange}></input>
