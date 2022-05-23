4a47bc7dd02883614ea8f08f3d0d0a701460f0d4
Nicolas Debray <97224385+chmick15@users.noreply.github.com>
2022-05-23 11:50:12 +0200

    responsive notes ok

--- src/components/projects/todo/notes/Notes.js
+++ src/components/projects/todo/notes/Notes.js
@@ -3,12 +3,14 @@ import React from "react";
 const Notes = ({ note, handleDelete }) => {
 
     return (
-        <div className="w-1/4 h-1/4 bg-gradient-to-t from-yellow-300 to-yellow-500 m-2">
+        <div className="bg-gradient-to-t from-yellow-300 to-yellow-500 m-2">
             <div className="flex justify-between">
-                <p className="text-2xl font-bold text-center">{note.author}</p>
-                <span className="cursor-pointer" onClick={handleDelete}>X</span>
+                <p className="text-2xl font-bold text-center py-2 px-2">{note.author}</p>
+                <span className="cursor-pointer text-red-600 text-2xl font-bold mr-2 items-center" onClick={handleDelete}>x</span>
             </div>
-            <p className="text-xl">{note.comment}</p>
+            <div className="p-5">
+                <p className="text-xl">{note.comment}</p>
+            </div>
         </div>
     )
 }
--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Notes from "../components/projects/todo/notes/Notes";
 
 const TodoList = () => {
-    const [notes, setNotes] = useState([{ id: 1, author: 'Nicolas', comment: 'This is a message' }, { id: 2, author: 'Pierre', comment: 'It is another message' }]);
+    const [notes, setNotes] = useState([{ id: 1, author: 'Todo', comment: 'This is a note' }, { id: 2, author: 'Todo', comment: 'Just fill the input, click the "Add note" button to add your note' }, { id: 3, author: 'Todo', comment: 'Click the Red Cross to delete a note' }]);
     const [newMess, setNewMess] = useState('')
 
     const handleChange = (e) => {
@@ -13,7 +13,7 @@ const TodoList = () => {
         e.preventDefault()
         const id = new Date().getTime()
         const comment = newMess
-        const author = 'À faire'
+        const author = 'Todo'
         notes.push({ id, comment, author })
         setNewMess('')
     }
@@ -27,13 +27,17 @@ const TodoList = () => {
 
     return (
         <div className="h-screen w-screen">
-            <h1>{notes.length}</h1>
-            {notes.length > 0 ? notes.map((note, id) => <Notes note={note} key={id} handleDelete={handleDelete} />) : <p>There's no note</p>}
-            <form>
-                <input type='text' placeholder="Note" onChange={handleChange} value={newMess}></input>
-                <button onClick={handleSubmit}>Add note</button>
-            </form>
-        </div>
+            <h1 className=" text-center text-3xl">I have {notes.length} notes</h1>
+            <div className="grid grid-cols-1">
+                <form>
+                    <input type='text' placeholder="Note" onChange={handleChange} value={newMess}></input>
+                    <button onClick={handleSubmit}>Add note</button>
+                </form>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 m-10">
+                {notes.length > 0 ? notes.map((note, id) => <Notes note={note} key={id} handleDelete={handleDelete} />) : <p>There's no note</p>}
+            </div>
+        </div >
     )
 };
 
